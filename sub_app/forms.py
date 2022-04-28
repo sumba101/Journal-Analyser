7bda0f8edba4a861476333f22d72140802d3b8ed
sumba101 <sumanth.goku@gmail.com>
2022-04-28 19:45:24 +0100

    Finished up the web app more

--- sub_app/forms.py
+++ sub_app/forms.py
@@ -1,17 +1,6 @@
 from django import forms
 
-class QuestionsForm( forms.Form ):
-    def __init__(self, *args, **kwargs):
-        self.count = kwargs.pop( 'count' )
-        self.qs= kwargs.pop('questions')
-        self.egs=kwargs.pop('examples')
-        self.topic=kwargs.pop('focus_word')
-        super(QuestionsForm, self).__init__(*args, **kwargs)
-        for i in range(self.count):
-            self.fields['{}) What is your answer for the section: {} under the topic {}' .format(i+1,self.qs[i],self.topic)] = forms.CharField()
-            self.fields['{}) What is your answer for the section: {} under the topic {}'.format( i+1,self.qs[i], self.topic )].help_text = "This is an example paragraph for the same section from a different topic <br/> <br/>"+self.egs[i]
 
-
-
-class TextInput(forms.Form):
-    focus_word= forms.CharField()
+class JournalEntry(forms.Form):
+    entry = forms.CharField(label="Make your diary entry",
+                                 widget=forms.Textarea(attrs={"rows": 50, "cols": 60}))
--- sub_app/views.py
+++ sub_app/views.py
@@ -1,23 +1,22 @@
 import pickle
-import time
+from datetime import datetime
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-import gensim.downloader as api
-from .forms import TextInput, QuestionsForm
+from .forms import JournalEntry
 
-# from .parser import getProcessedData
-focus_word=str()
-noOfSections=int()
-selected=list()
-candidate_content=list()
 
 def home(request):
     return render( request, 'sub_app/home.html')
 
 def save_obj(obj, name ):
-    with open('sub_app/AnswersData/'+ name + '.pkl', 'wb') as f:
+    with open('sub_app/entries_collected/' + name + '.pkl', 'rb') as f:
+        curr_data = pickle.load(f)
+#  Curr_data is a list of dicts, each dict contains "entry" and "datetime" as keys and values
+    curr_data.append(obj)
+
+    with open('sub_app/entries_collected/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name):
@@ -26,21 +25,17 @@ def load_obj(name):
 
 def journal(request):
     if (request.method == 'POST'):
-        form = QuestionsForm( request.POST, count=noOfSections, questions=selected, examples=candidate_content, focus_word=focus_word )
+        form = JournalEntry( request.POST)
         if form.is_valid():
-            dataset = dict()
-            for i,field in enumerate(selected):
-                dataset[field] = form.cleaned_data['{}) What is your answer for the section: {} under the topic {}' .format(i+1,field,focus_word)]
-
-            print(dataset)
-            save_obj(dataset,focus_word+time.ctime())
-            return HttpResponseRedirect( '/results/' )
+            form["datetime"] = datetime.now()
+            print(form.cleaned_data)
+            save_obj(form,"Anand")
+            return HttpResponseRedirect( '/' )
 
         else:
             print( form.errors )
 
-    form = QuestionsForm( count=noOfSections, questions=selected, examples=candidate_content,
-                      focus_word=focus_word )
+    form = JournalEntry()
     return render( request, 'sub_app/app.html',{'form':form} )
 
 
